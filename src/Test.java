a3d24df92285bce1e2cc8ad184bdd397cb3eb9ca
hidetaka kobayashi <longpat2010@yahoo.co.jp>
2017-04-09 18:18:14 +0900

    メソッドのテスト

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by hidetakakobayashi on 2017/04/09.
+ */
+public class Test {
+
+    /**
+     *
+     */
+    public String getKey () {
+
+        return "test";
+    }
+
+}
--- src/app.java
+++ src/app.java
@@ -1,11 +1,13 @@
 /**
  * Created by hidetakakobayashi on 2017/04/09.
  */
+
 public class app {
     public static void main (String[] args) {
         System.out.println("Hello World !!");
-        System.out.println("Hello World !!");
-        System.out.println("Hello World !!");
-        System.out.println("Hello World !!");
+        Test test = new Test();
+        System.out.println(test.getKey());
+
+
     }
 }
